e88b349179f318a849925f61af1ee2cd36f749f5
DukeManh <manhducdkcb@gmail.com>
2022-12-02 18:18:28 -0500

    Run today's challenge only

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
+use chrono::prelude::*;
+
 mod day_1;
 mod day_2;
 
 fn main() {
+    let challenges = [day1, day2];
+
+    let start_date = Utc.with_ymd_and_hms(2022, 12, 1, 0, 0, 0).unwrap();
+    let today = Utc::now();
+
+    let day_index = (today.day() - start_date.day()) as usize;
+    if day_index > challenges.len() - 1 {
+        panic!("Day {} hasn't started yet", day_index + 1)
+    }
+
+    let challenge = challenges[day_index as usize];
+    challenge();
+}
+
+fn day1() {
     println!(
         "Most calories carried by a single elf: {}",
         day_1::find_most_calories().unwrap()
@@ -11,7 +28,9 @@ fn main() {
         "Total calories carried by the top 3 elf: {}",
         day_1::find_total_calories_of_top_3().unwrap()
     );
+}
 
+fn day2() {
     println!(
         "Total score if I follow the assumed strategy: {}",
         day_2::get_total_score().unwrap()
